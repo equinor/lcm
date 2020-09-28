b2234d3029035709b9288f5c5c2993f98a717208
Stig Ofstad <stigofstad@gmail.com>
2020-09-28 14:01:10 +0200

    radix config

--- api/config.py
+++ api/config.py
@@ -4,7 +4,6 @@ import ast
 
 class Config:
     azure_table_passwd = os.getenv("AZURE_TABLE_PASSWD", "maf")
-    TABEL_CONNECTION_STRING = os.getenv("TABEL_CONNECTION_STRING")
     TABEL_ACCOUNT_NAME = os.getenv("TABEL_ACCOUNT_NAME", "lcmdevstorage")
     TABEL_KEY = os.getenv("TABEL_KEY")
     LOAD_TEST_DATA = ast.literal_eval(os.getenv("LOAD_TEST_DATA", "False"))
