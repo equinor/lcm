31e151ab6928675821e0985cef524b4e3746fd63
Stig Ofstad <stigofstad@gmail.com>
2020-11-23 14:02:24 +0100

    add production wsgi server

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 if [ "$1" = 'api' ]; then
-  flask run --host=0.0.0.0
+  if [ "$FLASK_ENV" = 'development' ]; then
+        flask run --host=0.0.0.0
+    else
+        gunicorn -b 0.0.0.0:5000 \
+            --worker-tmp-dir /dev/shm \
+            --capture-output \
+            --enable-stdio-inheritance \
+            --access-logfile=- \
+            --log-file=- \
+            --workers 4 \
+            --timeout 60 \
+            app:app
+    fi
 else
   exec "$@"
 fi
