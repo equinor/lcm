5433095cd0691fa438f540d61b0acaaf2cab8fed
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2021-01-13 15:17:56 +0100

    formatting from pre-commit hook

--- api/controllers/optimizer.py
+++ api/controllers/optimizer.py
@@ -85,5 +85,5 @@ def optimizer_request_handler(
             "products": weights["products"],
         },
         "curve": optimizer_result["curve"],
-        "bridgeScore": optimizer_result["bridge_score"] 
+        "bridgeScore": optimizer_result["bridge_score"],
     }
