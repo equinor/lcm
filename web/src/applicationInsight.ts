af79b1ecd633927ae77080847a99c45ac9ad8fd4
Mathias van der Bend <mvbe@equinor.com>
2025-02-26 15:19:23 +0100

    Appinsights in web + bicep file
    # Conflicts:
    #	web/package.json
    
    WIP - testing
    
    fixup: conditional log

---
+++ web/src/applicationInsight.ts
@@ -0,0 +1,20 @@
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const connectionString = import.meta.env.VITE_APPINSIGHTS_CONSTRING || ''
+
+export const reactPlugin = new ReactPlugin()
+export const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: connectionString,
+    enableAutoRouteTracking: false,
+    maxBatchInterval: 0,
+    extensions: [reactPlugin],
+  },
+})
+
+if (connectionString) {
+  appInsights.loadAppInsights()
+} else {
+  console.error('Application Insights connection string is not defined');
+}
