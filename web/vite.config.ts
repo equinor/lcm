f0fe4b52a5e1fa11bce86bf62a01280592ff492b
Mathias van der Bend <mvbe@equinor.com>
2026-03-03 11:46:24 +0100

    update vite.config for hotreloading

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -32,6 +32,12 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 80,
+    },
   },
   build: {
     outDir: 'build',
