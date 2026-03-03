ce8059cbc4f501041b5be78fc1539684e2ed9287
Sebastian Vittersø <svit@equinor.com>
2026-03-03 09:04:29 +0100

    fix: bug with using dataclasses on User class (illegal extra kwargs)

--- api/src/classes/user.py
+++ api/src/classes/user.py
@@ -6,3 +6,11 @@ class User:
     name: str
     oid: str
     roles: list[str] = field(default_factory=list)
+
+    @classmethod
+    def from_jwt(cls, jwt_payload: dict):
+        return cls(
+            name=jwt_payload.get("name", ""),
+            oid=jwt_payload.get("oid", ""),
+            roles=jwt_payload.get("roles", []),
+        )
