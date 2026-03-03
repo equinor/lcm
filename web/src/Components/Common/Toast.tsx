2151b73e99e6bee0a8cdf59308e0f08944efd1d5
Sebastian Vittersø <svit@equinor.com>
2026-03-03 13:54:57 +0100

    fix: simplify Toast.tsx

--- web/src/Components/Common/Toast.tsx
+++ web/src/Components/Common/Toast.tsx
@@ -1,29 +1,8 @@
 import { type Id, toast } from 'react-toastify'
 
 export function ErrorToast(msg: string, code?: number): Id {
-  const title = <p>Error</p>
   const config = { autoClose: 7000 }
 
-  if (code === 401)
-    return toast.error(
-      <>
-        {title}
-        <p>Session Expired</p>
-        <p>Please reload page</p>
-        <div style={{ textAlign: 'right' }}>
-          <small>{code}</small>
-        </div>
-      </>,
-      config
-    )
-  return toast.error(
-    <>
-      {title}
-      <p>{msg}</p>
-      <div style={{ textAlign: 'right' }}>
-        <small>{code}</small>
-      </div>
-    </>,
-    config
-  )
+  if (code === 401) return toast.error('Session Expired, please reload page', config)
+  return toast.error(msg, config)
 }
