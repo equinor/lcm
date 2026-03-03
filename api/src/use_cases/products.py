63c7c064fe7099e1dcd3a3fffcf5b27736323fbf
Sebastian Vittersø <svit@equinor.com>
2026-03-03 12:44:04 +0100

    refactor: add ProductDTO dataclass

--- api/src/use_cases/products.py
+++ api/src/use_cases/products.py
@@ -1,3 +1,6 @@
+import json
+from dataclasses import asdict, dataclass
+
 from cachetools import TTLCache, cached
 from flask import Response
 
@@ -5,32 +8,48 @@ from config import Config
 from util.azure_table import get_service
 
 
-def sort_products(products: dict[str, dict]) -> dict[str, dict]:
+@dataclass
+class ProductDTO:
+    id: str
+    title: str
+    supplier: str
+    co2: float
+    cost: float
+    environmental: str
+    sack_size: int
+    cumulative: list[float] | None
+
+    @classmethod
+    def from_db_entity(cls, entity) -> "ProductDTO":
+        cumulative = json.loads(entity.get("cumulative")) if entity.get("cumulative") else None
+        return cls(
+            id=entity.id,
+            title=entity.title,
+            supplier=entity.supplier,
+            co2=entity.co2,
+            cost=entity.cost,
+            environmental=entity.environmental,
+            sack_size=entity.sack_size,
+            cumulative=cumulative,
+        )
+
+
+def sort_product_dict(products: dict[str, ProductDTO]) -> dict[str, ProductDTO]:
     values = list(products.values())
-    values.sort(key=lambda p: (p["supplier"], p["id"]))
+    values.sort(key=lambda p: (p.supplier, p.id))
 
-    return {v["id"]: v for v in values}
+    return {v.id: v for v in values}
 
 
-def retrieve_products() -> dict[str, dict]:
+def retrieve_products() -> dict[str, ProductDTO]:
     products = get_service().query_entities(Config.PRODUCT_TABLE_NAME)
 
     products_response = {}
     for p in products:
-        products_response[p.RowKey] = dict(p)
-        del products_response[p.RowKey]["PartitionKey"]
-        del products_response[p.RowKey]["RowKey"]
-        del products_response[p.RowKey]["Timestamp"]
-        del products_response[p.RowKey]["etag"]
-        products_response[p.RowKey]["sackSize"] = p.sack_size
-        cumulative = (
-            [float(v) for v in p.get("cumulative").replace("[", "").replace("]", "").split(",")]
-            if p.get("cumulative")
-            else None
-        )
-        products_response[p.RowKey]["cumulative"] = cumulative
+        product_dto = ProductDTO.from_db_entity(p)
+        products_response[p.RowKey] = product_dto
 
-    sorted_products = sort_products(products_response)
+    sorted_products = sort_product_dict(products_response)
 
     if not len(sorted_products):
         raise ValueError("No products found in storage")
@@ -42,6 +61,6 @@ def retrieve_products() -> dict[str, dict]:
 def products_get():
     try:
         products_response = retrieve_products()
-        return products_response
+        return {k: asdict(v) for k, v in products_response.items()}
     except ValueError:
         return Response("Failed to fetch products from storage", 500)
