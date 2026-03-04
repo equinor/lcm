de0a1ca2375ec0bbaf8803428a0145b3614250a9
Sebastian Vittersø <svit@equinor.com>
2026-03-04 08:53:31 +0100

    chore: remove unused print_progress function

--- api/src/util/utils.py
+++ api/src/util/utils.py
@@ -1,12 +1,6 @@
 import re
 
 
-def print_progress(i: int, goal: int):
-    if i != 0 and i % 100 == 0:
-        progress = round(100 / goal * i, 2)
-        print(f"Progress: {progress}%")
-
-
 def camel_to_underscore(name):
     """Convert camelCase to snake_case."""
     camel_case_pattern = re.compile(r"([A-Z])")
